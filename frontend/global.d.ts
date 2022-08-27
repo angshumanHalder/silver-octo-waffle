de820a315d3589427acc89d84219821cdf2eee3e
Angshuman Halder <angshuman.halder243@gmail.com>
2022-08-27 21:04:06 +0530

    :truck: Moved files inside the frontend directory

--- frontend/global.d.ts
+++
@@ -1,8 +0,0 @@
-import { Contract, WalletConnection } from "near-api-js";
-declare global {
-  interface Window {
-    walletConnection: WalletConnection;
-    accountId: any;
-    contract: Contract;
-  }
-}
--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Polls from "../Polls/Polls";
 import { CreatePoll } from "../CreatePoll/CreatePoll";
 import Poll from "../Poll/Poll";
-import init, { greet } from "ring-sig";
+import init, { genSignature, genVoter } from "ring-sig";
 
 export default function App() {
   // if (!window.walletConnection.isSignedIn()) {
@@ -12,8 +12,18 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      await init();
-      greet();
+      init().then(() => {
+        const v1 = genVoter();
+        const v2 = genVoter();
+        const v3 = genVoter();
+        const v4 = genVoter();
+
+        console.log(v2.public);
+
+        const ring = [v2.public, v3.public, v4.public];
+        const sig = genSignature(v1.secret, v1.public, v1.image, ring, 1, "Hello");
+        console.log(sig);
+      });
     })();
   });
 
